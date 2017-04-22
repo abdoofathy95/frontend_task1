8ee8994694da838046f257b84ecd75de8280be79
Abdoo Fathy <abdoofathy95@gmail.com>
2017-04-22 03:28:01 +0200

    init

--- javascript/blog.js
+++
@@ -1,6 +0,0 @@
-$(".dropdown_l .dropdown_content a").click(function() {
-  $(".dropdown_l .dropdown_button .clr .dropdown_text_area").html(this.innerText);
-});
-$(".dropdown_r .dropdown_content a").click(function() {
-  $(".dropdown_r .dropdown_button .clr .dropdown_text_area").html(this.innerText);
-});
